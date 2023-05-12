afdf3ec8abe29d68c761d0c809809dedff07e406
josemalcher <contato@josemalcher.net>
2023-05-12 18:34:31 -0300

    10. Componente NavBar

--- social/src/App.vue
+++ social/src/App.vue
@@ -1,5 +1,16 @@
 <template>
-  <div id="app">
+  <div class="" id="app">
+    <nav>
+      <div class="nav-wrapper container">
+        <a href="#" class="brand-logo">Logo</a>
+        <ul id="nav-mobile" class="right hide-on-med-and-down">
+          <li><a href="sass.html">Sass</a></li>
+          <li><a href="badges.html">Components</a></li>
+          <li><a href="collapsible.html">JavaScript</a></li>
+        </ul>
+      </div>
+    </nav>
+
     <router-view/>
   </div>
 </template>
