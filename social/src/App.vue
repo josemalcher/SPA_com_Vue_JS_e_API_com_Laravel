e2bc5d046b057d27614d890167667aec99c3c872
josemalcher <contato@josemalcher.net>
2023-05-17 16:53:45 -0300

    11. Rodapé

--- social/src/App.vue
+++ social/src/App.vue
@@ -1,18 +1,33 @@
 <template>
-  <div id="app">
-    <NavBar cor="green" url="####" logo="Logo so Site Aqui" />
-
-    <router-view/>
-  </div>
+  <span>
+    <header>
+      <NavBar cor="green darken-1" url="####" logo="Logo so Site Aqui"/>
+    </header>
+    <main>
+      <div class="container">
+        <router-view/>
+      </div>
+    </main>
+    <footer>
+      <FooterVue cor="green darken-1" logo="Logo so Site Aqui" descricao="descrição do site">
+            <li><a class="grey-text text-lighten-3" href="#!">Link 1</a></li>
+            <li><a class="grey-text text-lighten-3" href="#!">Link 2</a></li>
+            <li><a class="grey-text text-lighten-3" href="#!">Link 3</a></li>
+            <li><a class="grey-text text-lighten-3" href="#!">Link 4</a></li>
+      </FooterVue>
+    </footer>
+  </span>
 </template>
 
 <script>
 import NavBar from "./components/layout/NavBar.vue";
+import FooterVue from "./components/layout/FooterVue.vue";
 
 export default {
   name: 'App',
   components: {
-    NavBar
+    NavBar,
+    FooterVue
 
   }
 }
---
+++ social/src/components/layout/FooterVue.vue
@@ -0,0 +1,41 @@
+<template>
+  <footer :class="cor + 'page-footer'">
+    <div class="container">
+      <div class="row">
+        <div class="col l6 s12">
+          <h5 class="white-text">{{ logo || 'Site teste'}}</h5>
+          <p class="grey-text text-lighten-4">{{descricao || 'Descrição Teste'}}</p>
+        </div>
+        <div class="col l4 offset-l2 s12">
+          <h5 class="white-text">Links</h5>
+          <ul>
+            <slot></slot>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <div class="footer-copyright">
+      <div class="container">
+        © 2014 Copyright Text
+        <a class="grey-text text-lighten-4 right" href="#!">More Links</a>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<style>
+
+</style>
+
+<script>
+export default {
+  name: 'Footer',
+  props: ['cor', 'logo', 'descricao'],
+
+  data() {
+    return {
+    }
+  }
+}
+</script>
+
