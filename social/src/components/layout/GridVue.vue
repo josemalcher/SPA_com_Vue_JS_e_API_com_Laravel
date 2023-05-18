de15294440e7db51cbe119657e7c7b2646b220e5
josemalcher <contato@josemalcher.net>
2023-05-18 19:19:59 -0300

    13. Preparando o Menu Lateral

---
+++ social/src/components/layout/GridVue.vue
@@ -0,0 +1,18 @@
+<template>
+  <div :class="'col s'+ tamanho">
+    <slot></slot>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
+<script>
+export default ({
+  name: "GridVue",
+  props: ['tamanho'],
+  data(){
+
+  }
+})
+</script>
