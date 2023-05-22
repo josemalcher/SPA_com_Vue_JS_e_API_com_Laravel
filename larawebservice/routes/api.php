89379dabcae067e5959add410a0091d126579920
josemalcher <contato@josemalcher.net>
2023-05-22 18:01:53 -0300

    32. Método de Login na API

--- larawebservice/routes/api.php
+++ larawebservice/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,6 +14,21 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+
+Route::post('/register', [\App\Http\Controllers\AuthController::class, 'register']);
+Route::post('/login', [\App\Http\Controllers\AuthController::class, 'login']);
+
+//Route::post('/cadastro', function (Request $request) {
+//    $data = $request->all();
+//    $user = User::create([
+//        'name' => $data['name'],
+//        'email' => $data['email'],
+//        'password' => bcrypt($data['password'])
+//    ]);
+//    $user->token = $user->createToken($user->email)->accessToken;
+//
+//    return $user;
+//});
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
