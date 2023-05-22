7bb11096a3a519cca5fb56f1e24dc18679c4c2db
josemalcher <contato@josemalcher.net>
2023-05-22 20:57:34 -0300

    33. Http com Axios no Vue JS

--- social/src/pages/login/Login.vue
+++ social/src/pages/login/Login.vue
@@ -9,9 +9,9 @@
     <span slot="principal">
         <h2>Login</h2>
 
-        <input type="text" placeholder="Email" value="">
-        <input type="password" placeholder="Senha" value="">
-        <button type="button" class="btn">Entrar</button>
+        <input type="text" placeholder="Email" v-model="email">
+        <input type="password" placeholder="Senha" v-model="password">
+        <button type="button" class="btn" @click="login()">Entrar</button>
       <router-link class="btn orange" to="/cadastro">Cadastrar</router-link>
 
     </span>
@@ -21,12 +21,33 @@
 
 <script>
 import LoginTemplate from "@/templates/LoginTemplate.vue";
-
+import axios from 'axios'
 export default {
   name: "Login",
   components: {
     LoginTemplate,
   },
+  data() {
+    return {
+      email: '',
+      password: '',
+    }
+  },
+  methods: {
+    login() {
+      console.log('OKOK')
+      axios.post('http://localhost/api/login', {
+        email: this.email,
+        password: this.password
+      }).then(response => {
+        console.log(response)
+      }).catch(e => {
+        this.errors.push(e)
+      });
+
+
+    },
+  }
 }
 </script>
 
